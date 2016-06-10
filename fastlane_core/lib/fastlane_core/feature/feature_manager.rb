f0d6e1f9661b391a017d827e8a15fb81ccc010b7
David Ohayon <ohayon.1@gmail.com>
2016-06-10 11:44:06 -0400

    stop barking at me hound!

--- fastlane_core/lib/fastlane_core/feature/feature_manager.rb
+++ fastlane_core/lib/fastlane_core/feature/feature_manager.rb
@@ -1,16 +1,19 @@
 module FastlaneCore
   class FeatureManager
-    @@enabled_features = []
+    class << self
+      attr_accessor :enabled_features
+    end
+    @enabled_features = []
 
     def self.experiments_enabled?
       return ENV['FASTLANE_ENABLE_ALL_EXPERIMENTS']
     end
 
     def self.enabled?(key)
-      feature = features.find { |feature| feature.key == key }
+      feature = features.detect { |feat| feat.key == key }
       return false if feature.nil?
       return true if experiments_enabled? && feature.experiment == true
-      return @@enabled_features.include?(key) || ENV[feature.env_var]
+      return @enabled_features.include?(key) || ENV[feature.env_var]
     end
 
     def self.register_class_method(klass:, symbol:, default_symbol:, override_symbol:, key:)
@@ -42,7 +45,7 @@ module FastlaneCore
     end
 
     def self.enable!(key)
-      @@enabled_features << key unless @@enabled_features.include?(key)
+      @enabled_features << key unless @enabled_features.include?(key)
     end
   end
 end
